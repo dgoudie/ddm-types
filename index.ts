25c9f1ad8339db32b4b4454047885989ae8bb516
dgoudie <34222208+dgoudie@users.noreply.github.com>
2021-03-27 19:02:00 -0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+export interface BeerOrLiquorBrand {
+    _id: string;
+    name: string;
+    type:
+        | 'BEER'
+        | 'WINE'
+        | 'CIDER'
+        | 'MEAD'
+        | 'SAKE'
+        | 'GIN'
+        | 'BRANDY'
+        | 'WHISKEY'
+        | 'RUM'
+        | 'TEQUILA'
+        | 'VODKA'
+        | 'ABSINTHE'
+        | 'EVERCLEAR'
+        | 'OTHER';
+    inStock: boolean;
+}
+
+export interface MixedDrinkRecipe {
+    _id: string;
+    name: string;
+    requiredBeersOrLiquors: string[];
+}
